7d05fe1d08dfd9ff771c09d5323ddc271857f3a6
Иван <ivkin2003@list.ru>
2024-08-22 00:08:14 +0300

    form modified

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def index():
     if request.method == 'POST':
         name = request.form.get('name')
         tel = request.form.get('telephone')
-        message = request.form.get('message')
-        sendEmail(mail, render_template('mail.html', name = name, telephone = tel, message = message), "Новая заявка")
+        sendEmail(mail, render_template('mail.html', name = name, telephone = tel), "Новая заявка")
         flash('Заявка отправлена')
     return render_template('index.html')
 
